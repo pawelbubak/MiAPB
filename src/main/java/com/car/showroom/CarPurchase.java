e1abc44c72fe15fb1ce4928b8bf8465f49947832
pawelbubak <pawelbubak97@gmail.com>
2021-01-03 05:54:11 -0500

    Init camunda project

---
+++ src/main/java/com/car/showroom/CarPurchase.java
@@ -0,0 +1,9 @@
+package com.car.showroom;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Car purchase app")
+public class CarPurchase extends ServletProcessApplication {
+
+}
