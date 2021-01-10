9705bcca6ea689470690fd8a6dbaf48e1b8c3408
pawelbubak <pawelbubak97@gmail.com>
2021-01-10 12:22:24 -0500

    Fixes

---
+++ src/main/java/com/car/showroom/CarOrder.java
@@ -0,0 +1,9 @@
+package com.car.showroom;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Car order app")
+public class CarOrder extends ServletProcessApplication {
+
+}
